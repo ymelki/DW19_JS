0f1c8e7ebb622df40a11b7cbe7aaea8fe2a1c78a
Yoel Melki <ymelki@gmail.com>
2022-06-27 16:31:46 +0200

    first commit

--- script.js
+++ script.js
@@ -36,10 +36,37 @@ console.error("Nous avons du SEL en Q. de " + SEL + ". Nous avons du sucre en qu
 // VOTRE CODE INVERSE LES VARIABLES
 
 // CREEZ UN USTENSIL / VARIABLE QUI SERT DE MEMOIRE
-TEMP=SEL // TEMP SAUVEGARDE AVEC LA VALEUR DE SEL
-SEL=SUCRE // SEL ON PEUT L ECRASER AVEC LA DE SUCRE
-SUCRE=TEMP // SUCRE ON PEUT GARDER LA VALEUR DE SEL
+TEMP=SEL; // TEMP SAUVEGARDE AVEC LA VALEUR DE SEL
+SEL=SUCRE; // SEL ON PEUT L ECRASER AVEC LA DE SUCRE
+SUCRE=TEMP;// SUCRE ON PEUT GARDER LA VALEUR DE SEL
  // SEL NE PAS CONTENIR LA VALEUR DU SUCRE INITIAL CAR IL A ETE ECRASE
 
-console.error("Nous avons du SEL en Q. de " + SEL + ". Nous avons du sucre en quantité de " + SUCRE)
+console.error("Nous avons du SEL en Q. de " + SEL + ". Nous avons du sucre en quantité de " + SUCRE);
 // Nous avons du SEL en Q. de 80  Nous avons du sucre en quantité de 100
+
+/*
+EXO 2 :
+1. Recuperer l'age de l'utilisateur via la commande prompt
+2. Affichez l'age de l'utilisateur dans la console
+3. Affiche quel age aura l'utilisateur dans 2 ans dans la console
+Si 25 est inséré dans la console il sera écrit
+L'utilisateur aura 27 ans dans 2 ans
+*/
+
+
+
+
+// 1 PROMPT 
+age = prompt("Veuillez entré votre âge ?"); // variable age va contenir le texte entré par l'uti.
+console.error(typeof(age)) // affiche dans la console la donnée qui est entrée. TYPEOF LE TYPE DE VARIABLE
+// 2  Affichez l'age de l'utilisateur dans la console
+// Afficher dans la console : L'utilisateur à 25 ans.
+console.error("L'utilisateur à " + age + " ans");
+//3  Affichez
+AGEPLUSDEUX=parseInt(age)+2 // CONVERTIR UNE CHAINE DE CARACTERE EN NOMBRE
+console.error("L'utilisateur aura " + AGEPLUSDEUX + " ans")
+
+
+A=25;
+B=A+2
+console.error(B); // 27
