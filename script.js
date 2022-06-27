a2d6073b1296eef45ebc80d03b07455b9a2042e0
Yoel Melki <ymelki@gmail.com>
2022-06-27 14:15:52 +0200

    first commit

--- script.js
+++ script.js
@@ -7,7 +7,23 @@ Nous allons apprendre
 4. LES FONCTIONS
 */
 
-// Les variables
+//  Les variables
 
+/* 
 
-console.error("test");
+1. Pourquoi les variables ?   pour stocker des données    EX: Nom d'un jeu = "jeu du plus ou du moins";
+2. Une variable : le nom de la variable et la valeur de la variable : Tiroir qui porte un nom
+EX: Tiroir A <= 10 Valeur  
+Le numérique ne contient pas de guillement
+Les chaines de caracteres  contient  des guillements
+Additionner des nombes ca fonctionnent
+Addition une chaine de caractere avec autre choses fait de la concetanation
+*/
+a=10 // 10 rentre dans la variable A
+b=50 // PAREIL
+c="80" // PAREIL
+b=90 // B va contenir 90
+
+console.error(a+parseInt(c));
+console.error(typeof(c));
+console.error(b);
