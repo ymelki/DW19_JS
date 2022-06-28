f743200a4e58db40556dfc231aacb1ef62f45505
Yoel Melki <ymelki@gmail.com>
2022-06-28 11:26:13 +0200

    first commit

--- script3.js
+++ script3.js
@@ -8,19 +8,41 @@
 
 //BOUCLE FOR QUI INITIALISE UN COMPTEUR QUE L ON A NOMME I QUI DEMMARE A 0 ET TERMINE A 5
 for (i=0;i<5;i++){
-    console.error(i)
+   // console.error(i)
     // affiche une alerte I FOIS
-    alert("test") 
+   // alert("test") 
 }
 
 /**
  * EXO 1 : Afficher les nombre de 1 à 100 dans la console
  */
-
+for (i=0; i<100; i++){
+   // console.error(i)
+}
 /**
  * EXO 2 : Afficher les nombre de 100 à 200 dans la console
  */
 
+for (i=100;i<200;i++){
+    // console.error(i)
+}
+
 /**
  * EXO 3 : Afficher les table multiplication de 2 dans la console
+ */
+
+for (i=1;i<11; i++){
+    result=i*2
+    console.error("2*"+i+"="+result)
+}
+
+/**
+ * 10 * le fait d'afficher dans la table
+ * 
+ * 
+ * 2*1=2
+ * 2*2=4
+ * 2*3=6
+ * 
+ * 
  */
