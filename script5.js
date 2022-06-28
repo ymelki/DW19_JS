29ac0adcfd66704b067bab189cb7d6d5ad72a0e5
Yoel Melki <ymelki@gmail.com>
2022-06-28 16:55:46 +0200

    first commit

--- script5.js
+++ script5.js
@@ -44,5 +44,18 @@ function mafonction(){
  * EXO 6
  * ECRIRE une fonction qui affiche dans la div ce qu il y a dans le champs texte .
  * Cette fonction se declenche au moment ou vous tapez du texte dans le champs texte
+ * 
+ * EXO 7
+ *  * CREEZ UNE FONCTION QUI AFFICHE  DANS LA DIV UN NUMERO QUI S INCREMENTE DE 1 A CHAQUE FOIS QUE LON CLIQUE SUR LE BOUTON
+ * 
+ *  * EXO 8
+ *  * CREEZ UNE FONCTION QUI AFFICHE DANS LA DIV SI L AGE DANS LE TEXTE EST > 18 OU < EN ECRIVANT MAJEUR OU MINEUR
  */
+// EX 6
+function mafonction2(){
+    document.getElementById("madiv").innerHTML=document.getElementById("montexte").value
+}
+document.getElementById("montexte").addEventListener(  "input", mafonction2       )
 
+document.getElementById("monboutton").addEventListener(  "click", mafonction       )
+document.getElementById("boutton").addEventListener(  "click", alert2       )
