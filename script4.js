ee2dedef6b09ee7885e58a5581e70a9ff7336f35
Yoel Melki <ymelki@gmail.com>
2022-06-28 13:57:15 +0200

    first commit

--- script4.js
+++ script4.js
@@ -1,4 +1,21 @@
 /**
  * FONCTIONS
  * 
+ * 
+ * parsint : FONCTION METHODE JS CONVERTIT TEXTE EN NUMERIQUE
  */
+
+// CREATION DE LA FONCTION QUE L ON DECRIT
+function mafonction(){
+    console.error("je teste ma premiere fonction ! ")
+}
+// appel
+mafonction()
+
+// EXO 1 : CREEZ UNE FONCTION QUI AFFICHE UNE ALERT HELLO WORLD
+/* EXO 2 : 1. Recuperer l'age de l'utilisateur via la commande prompt
+2. Affichez l'age de l'utilisateur dans la console
+3. Affiche quel age aura l'utilisateur dans 2 ans dans la console
+Si 25 est inséré dans la console il sera écrit
+L'utilisateur aura 27 ans dans 2 ans
+*/
