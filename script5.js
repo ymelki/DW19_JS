7a5144e227be47e2ea39e42586251e78248683c5
Yoel Melki <ymelki@gmail.com>
2022-06-28 15:29:51 +0200

    first commit

---
+++ script5.js
@@ -0,0 +1,26 @@
+function mafonction(){
+   alert("vous avez cliqué sur le boutton ! ");
+   document.getElementById("montexte").value="LA VALEUR QUI M INTERESSE  !"
+   document.getElementById("monboutton").value="BOUTTON CLIQUE   !"
+   document.getElementById("madiv").innerHTML="ma div a ete changé ! "
+}
+
+console.error(document.getElementById("montexte"))
+console.error(document.getElementById("monboutton"))
+console.error(document.getElementById("madiv"))
+
+/**
+ * 
+ * 1. RECUPERER UN OBJET DE LA PAGE HTML 
+ * document
+ * RECUPERER UN DES CHAMP DE CETTE OBJET
+ * getElementById("identifiantcorrespondant")
+ * Modifier le html
+ * .laproprietehtml = valeur // on affecte une valeur a cette propriete
+ * 
+ * 
+ * Modifier le html d'une div on utilier innerHTML
+ */2. 
+
+
+
