ba86c29b6fd9bac4fd9ab356df45b3080957f47c
Yoel Melki <ymelki@gmail.com>
2022-06-30 13:54:43 +0200

    first commit

--- projet.js
+++ projet.js
@@ -5,3 +5,17 @@ function entierAleatoire(min, max) {
 //La variable contient un nombre aléatoire compris entre 1 et 10
 var entier = entierAleatoire(1, 100);
 console.log(entier)
+/**
+ * RECUPERER LA FONCTION
+ * ET AFFICHER DANS LA LOG UN NOMBRE ENTRE 1 ET 10 .. ENTRE 1 ET 100 .. ENTRE 5 ET 15
+ * 
+ * CREER UNE FONCTION QUI VA RENVOYER UN NOMBRE ENTRE 1 et un parametre de la fonction
+ * 
+ * 
+ * EXO 1
+ * CREER UNE FONCTION QUI VA RENVOYER UN TABLEAU IL VA CONTENIR 10 NOMBRE A L INTERIEUR . IL SERONT ALEATOIRE ENTRE 1 ET 100
+ * 
+ *  * EXO 2
+ * CREER UNE FONCTION QUI VA RENVOYER UN TABLEAU IL VA CONTENIR ENTRE 1 ET 100 NOMBRE A L INTERIEUR  . IL SERONT ALEATOIRE ENTRE 1 ET 100
+ * 
+ */
