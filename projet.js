a18744935f1b8667811e42bc362eb8c96ccf668c
Yoel Melki <ymelki@gmail.com>
2022-06-30 16:27:27 +0200

    first commit

--- projet.js
+++ projet.js
@@ -75,6 +75,8 @@ console.table(tabfalea())
  * EXO 1 Creer une fonction qui va utiliser la fonction deja creer precedemement en 1. vous aller renvoyer le nombre max du tableau
  * EXO 2 reer une fonction qui va utiliser la fonction deja creer precedemement en 1. On va imaginer qu on a des âges. La fonction renvoie
  * le nombre de personne majeur > 18 ANS
+ * EXO 3. Creer une fonction qui va utiliser la fonction deja creer precedemement en 1.  On va imaginer qu on a des prix HT. La fonction renvoyé 
+ * le tableau avec les même prix en TTC : prix * 1.2
  * 
 */
 
